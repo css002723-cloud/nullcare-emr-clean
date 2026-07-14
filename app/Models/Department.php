662bfba2837aba82d18ff9c1597d8fe882ed0ac9
Phill <css002723@must.ac.mw>
2026-07-14 13:05:12 +0200

    Create Department model with proper relationships

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,23 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Department extends Model
 {
-    protected $fillable = ['name', 'type'];
+    use HasFactory;
+
+    protected $fillable = ['name'];
+    public $timestamps = false;
 
     public function users()
     {
         return $this->hasMany(User::class);
-    }
-
-    public function appointments()
-    {
-        return $this->hasMany(Appointment::class);
-    }
-
-    public function encounters()
-    {
-        return $this->hasMany(Encounter::class);
     }
 }
